04f441532e99b2981d6a2e9583c7ccf9863c1d00
SushilG96 <guptarahul201010@gmail.com>
2020-06-19 03:19:52 +0530

    Adding sol for H_Index_II

---
+++ H_Index_II.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+            citations.sort(reverse=True)
+            for x in range(len(citations),0, -1):
+                print(citations[x-1],x)
+                if citations[x-1] >= x:
+                    return x
+            return 0
+
