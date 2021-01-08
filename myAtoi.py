1d31ac0173c8859f10ea69b87b9b5a45e5ac672a
susgupta <susgupta@redhat.com>
2021-01-08 17:51:43 +0530

    myAtoi

---
+++ myAtoi.py
@@ -0,0 +1,25 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+            sign = 1
+            num = 0
+
+            if not s: return 0
+            s = s.strip()
+
+            # sign
+            if s and s[0] in '+-':
+                sign = 1 if s[0] == '+' else -1
+                s = s[1:]
+            num = 0
+            for ch in s:
+                if ch.isdigit():
+                    num = num * 10 + int(ch)
+                else:
+                    break
+            res = (num * sign)
+            if res >= (2 ** 31):
+                return (2 ** 31) -1
+            elif res < -(2 ** 31):
+                return -(2 ** 31)
+
+            return res
