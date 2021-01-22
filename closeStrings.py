4858c998dd54daaafd408bd49aea9a5b6253bfd5
susgupta <susgupta@redhat.com>
2021-01-23 02:50:04 +0530

    Add sol closeStrings.py

---
+++ closeStrings.py
@@ -0,0 +1,11 @@
+"""
+Input: word1 = "cabbba", word2 = "abbccc"
+Output: true
+Explanation: You can attain word2 from word1 in 3 operations.
+Apply Operation 1: "cabbba" -> "caabbb"
+Apply Operation 2: "caabbb" -> "baaccc"
+Apply Operation 2: "baaccc" -> "abbccc"
+"""
+class Solution:
+    def closeStrings(self, w1, w2):
+        return set(w1) == set(w2) and Counter(Counter(w1).values()) == Counter(Counter(w2).values())
