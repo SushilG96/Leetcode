0437b995c0a50a584a309e69fd58278b6c22aa7b
susgupta <susgupta@redhat.com>
2021-02-16 01:15:32 +0530

    Add sol

---
+++ kWeakestRows.py
@@ -0,0 +1,24 @@
+"""
+Input: mat = 
+[[1,1,0,0,0],
+ [1,1,1,1,0],
+ [1,0,0,0,0],
+ [1,1,0,0,0],
+ [1,1,1,1,1]], 
+k = 3
+Output: [2,0,3]
+Explanation: 
+The number of soldiers for each row is: 
+row 0 -> 2 
+row 1 -> 4 
+row 2 -> 1 
+row 3 -> 2 
+row 4 -> 5 
+Rows ordered from the weakest to the strongest are [2,0,3,1,4]
+
+"""
+z = {}
+for i,x in enumerate(mat):
+    z[i] = x.count(1)
+return sorted(z, key=z.get)[:k]
+
