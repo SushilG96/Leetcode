e27cbbfc4379eac46919676d887790c0acc25f0a
SushilG96 <susgupta@redhat>
2021-02-19 02:47:56 +0530

    Add sol

---
+++ numberOfArithmeticSlices.py
@@ -0,0 +1,24 @@
+"""
+A = [1, 2, 3, 4]
+
+return: 3, for 3 arithmetic slices in A: [1, 2, 3], [2, 3, 4] and [1, 2, 3, 4] itself.
+"""
+class Solution:
+    def numberOfArithmeticSlices(self, A: List[int]) -> int:
+        n = len(A)
+        dp = [None]*(n-1)
+
+        for i in range(1,n):
+            dp[i-1] = A[i] - A[i-1]
+
+        output = 0
+        m = 1
+
+        for i in range(1, len(dp)):
+            if dp[i] == dp[i-1]:
+                output += m
+                m += 1
+            else:
+                m = 1
+        return(output)
+
