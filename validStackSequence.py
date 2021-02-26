2ec1d9dcae65d9f87b912a7600a2636a3120cd47
SushilG96 <susgupta@redhat.com>
2021-02-27 03:35:05 +0530

    Add solution

---
+++ validStackSequence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack = []
+        pos = 0
+        for x in pushed:
+            stack.append(x)
+            while stack and stack[-1] == popped[pos]:
+                pos+=1
+                stack.pop()
+        return(stack == [])
