f7333bf23e845f0b59a10c99606a7a8459502070
SushilG96 <susgupta@redhat.com>
2021-03-31 19:31:31 +0530

    Add changes

--- src/minSteps_sol_2.py
+++ src/minSteps_sol_2.py
@@ -1,19 +1,20 @@
-dictn = {}
+def minSteps(s, t):
+    dictn = {}
 
-for i in s:
-	if i in dictn:
-		dictn[i] += 1
-	else:
-		dictn[i] = 1
+    for i in s:
+        if i in dictn:
+            dictn[i] += 1
+        else:
+            dictn[i] = 1
 
-count = 0
-for i in t:
-	if i not in dictn:
-		count += 1
-	else:
-		if dictn[i] == 0:
-			count += 1
-		else:
-			dictn[i] -= 1
+    count = 0
+    for i in t:
+        if i not in dictn:
+            count += 1
+        else:
+            if dictn[i] == 0:
+                count += 1
+            else:
+                dictn[i] -= 1
 
-return count
+    return count
