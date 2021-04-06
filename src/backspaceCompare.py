c924dd2c4f605ba5b13046face0dea8999678613
SushilG96 <susgupta@redhat.com>
2021-04-06 19:37:10 +0530

    Add sol

---
+++ src/backspaceCompare.py
@@ -0,0 +1,15 @@
+def _abc(self, s):
+    k = []
+    for x in s:
+        if x != '#':
+            k.append(x)
+        else:
+            if k:
+                k.pop()
+    return k
+
+
+class Solution:
+
+    def backspaceCompare(self, S: str, T: str) -> bool:
+        return _abc(self, S) == _abc(self, T)
