e4e5783ca36680dd2de27afa5a5471b20ca6f3d0
SushilG96 <susgupta@redhat.com>
2021-04-07 19:12:41 +0530

    Add sol

---
+++ src/arrangewords.py
@@ -0,0 +1,16 @@
+class Solution:
+
+    def arrangeWords(self, text: str) -> str:
+        test = text.split(" ")
+        test.sort(key=len)
+        res = []
+        for i, x in enumerate(test):
+            if i == 0:
+                if x[0].islower():
+                   x = x[0].upper() + x[1:]
+                res.append(x)
+            else:
+                if x[0].isupper():
+                   x = x[0].lower() + x[1:]
+                res.append(x)
+        return(' '.join(res))
